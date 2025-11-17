1d3fee3f590f6fa25fccd879e43707e63ad9f270
Eber_mk <ebercastano35@gmail.com>
2025-11-16 23:35:02 -0600

    Visualizar los datos de la BD

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
 import express from "express";
 import morgan from "morgan";
 import ejs from "ejs";
+import path from "path";
 import studentsRouter from "./routes/students.routes.js";
+import studentControllers from "./controllers/students.controllers.js";
 
 const app = express();
 
 //settings
 app.set("port", process.env.PORT||3000);
-app.set("view engine", "ejs");
-app.set("views","./src/views")
+//app.set("view engine", "ejs");
+//app.set("views","./src/views")
 
 //middleware
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: false}));
 
+app.set('views', path.join(path.resolve(), 'src/views')); 
+app.set('view engine', 'ejs');
+app.get('/', studentControllers.getAll);
 
-// 🟢 AQUÍ VA app.get('/') PARA CORREGIR EL 404
-app.get('/', (req, res) => {
-    // Debes pasar la lista de estudiantes para que index.ejs funcione
-    const students = []; // Reemplaza esto con la consulta real a la BD
-    res.status(200).render('index', { 
-        students: students, 
-        title: 'School Control'
-    });
+
+app.use((req, res, next) => {
+    res.status(404).render('404'); // Si tienes la vista 404.ejs
+    // O res.status(404).json({ message: "Ruta no encontrada" });
 });
 
 //routes
