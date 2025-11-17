6869031f45b525567a4fedf0025421b1d521092f
Eber_mk <ebercastano35@gmail.com>
2025-11-16 22:26:25 -0600

    Cambio de la base de datos

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-//const uri = "mongodb+srv://Eber_Mk:eber1235@ebermk.3nvangk.mongodb.net/school_control?retryWrites=true&w=majority&appName=EberMK";
+const uri = "mongodb+srv://Eber_Mk:eber1235@ebermk.3nvangk.mongodb.net/school_control?retryWrites=true&w=majority&appName=EberMK";
 // URL de connection local
-const uri = "mongodb://localhost:27017/school_control";
+//const uri = "mongodb://localhost:27017/school_control";
 
 
 mongoose.connect(uri)
