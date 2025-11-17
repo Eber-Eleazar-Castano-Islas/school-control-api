f4d764078348513663eed8a478ea8d54e9dd5738
Eber_mk <ebercastano35@gmail.com>
2025-11-16 23:00:44 -0600

    Intento 1 de areglar el GET 404

--- src/app.js
+++ src/app.js
@@ -15,6 +15,17 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: false}));
 
+
+// 🟢 AQUÍ VA app.get('/') PARA CORREGIR EL 404
+app.get('/', (req, res) => {
+    // Debes pasar la lista de estudiantes para que index.ejs funcione
+    const students = []; // Reemplaza esto con la consulta real a la BD
+    res.status(200).render('index', { 
+        students: students, 
+        title: 'School Control'
+    });
+});
+
 //routes
 app.use("/api/students",studentsRouter);
 
